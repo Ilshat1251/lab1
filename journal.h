1e33089cb73e28aa18222561bf53d85964d53f3e
Ilshat1251 <60716039+Ilshat1251@users.noreply.github.com>
2020-11-16 16:25:51 +0300

    Create journal.h

---
+++ journal.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include "journaldata.h"
+#include "MyVector.h"
+
+// Коллекция Одномерный массив (Вектор)
+class Journal {
+public:
+    Journal() = default;
+    Journal(size_t size);
+//   Journal(const Journal &oldJournal);
+//    Journal(Journal &&oldJournal);
+  //    Journal(const Journal& other);
+
+
+    void AddData(const JournalData &data);
+    void DeleteData(int index);
+
+    const JournalData &GetData(int index) const;    // константная версия ссылки на элемент
+    JournalData &GetData(int index);
+
+    void ClearAll();
+    int Size() const;
+    JournalData operator[](std::size_t index);
+    JournalData operator[](std::size_t index) const;
+
+    void SaveToFile(const string &filename);
+    void LoadFromFile(const string &filename);
+
+    void CheckJournal() const;
+    void operator=(const Journal& jd);
+
+private:
+    MyVector<JournalData> collection;
+};
+
+ostream &operator<<(ostream &os, const Journal&J);
